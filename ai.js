503bd4da998b546021427014b5c219a274a2b73a
chachin2 <chachgabriel624@gmail.com>
2026-04-14 17:05:57 -0600

    Implement weather agent using Langchain and Zod

---
+++ ai.js
@@ -0,0 +1,25 @@
+// First install: npm install langchain @langchain/anthropic
+import { z } from "zod";
+import { createAgent, tool } from "langchain";
+
+const getWeather = tool(
+  ({ city }) => `It's always sunny in ${city}!`,
+  {
+    name: "get_weather",
+    description: "Get the weather for a given city",
+    schema: z.object({
+      city: z.string(),
+    }),
+  },
+);
+
+const agent = createAgent({
+  model: "anthropic:claude-sonnet-4-6",
+  tools: [getWeather],
+});
+
+console.log(
+  await agent.invoke({
+    messages: [{ role: "user", content: "What's the weather in Tokyo?" }],
+  })
+);
